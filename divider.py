9e3ad16a4c95ee23ea8e0182d06f719d9cf6ef96
ThatCoder23 <51125629+ThatCoder23@users.noreply.github.com>
2019-10-14 15:28:04 -0600

    Add files via upload

---
+++ divider.py
@@ -0,0 +1,19 @@
+# Youssef Yatribi
+# CSC119003 Intro To Programming
+# February 01, 2018 
+
+# Sixth Decimal Division Program
+# This program will divide two numbers that are inputted in this
+
+# program greeting
+print('Hello this program divides two numbers to the sixth decimal point')
+
+# prompt the user for two numbers
+num_1 = int(input('Enter your first number:'))
+num_2 = int(input('Enter your second number:'))
+
+# divide the two numbers
+num_3 = num_1 / num_2
+
+# display result
+print('Your answer is:', format(num_3, '.6f'))
